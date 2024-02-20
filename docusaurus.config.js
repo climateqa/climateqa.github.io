210050388106f8cd9bf503f66ceaf55841b47d31
Théo ALVES DA COSTA <theo.alvesdacosta@ekimetrics.com>
2024-02-20 07:51:14 +0100

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -25,6 +25,7 @@ const config = {
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
+  deploymentBranch:'main',
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
